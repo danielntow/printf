745dd4b3b903317328cee6d2b5926336df91788e
danielntow <danielntow@gmail.com>
2023-08-19 19:35:24 +0000

    _printf.c

--- _printf.c
+++ _printf.c
@@ -2,7 +2,6 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <unistd.h>
-
 /**
  * _printf - Custom printf function
  * @format: Format string
@@ -14,64 +13,51 @@ int _printf(const char *format, ...)
 	va_list args;
 
 	va_start(args, format);
+
 	while (*format)
 	{
 		if (*format == '%')
 		{
 			format++;
-			switch (*format)
-			{
-			case 'c':
-			{
-				int arg;
 
-				arg = va_arg(args, int);
+			if (*format == 'c')
+			{
+				int arg = va_arg(args, int);
+
 				write(1, &arg, 1);
-				break;
 			}
-			case 's':
+			else if (*format == 's')
 			{
-				char *arg;
+				char *arg = va_arg(args, char*);
 
-				arg = va_arg(args, char*);
 				while (*arg)
 				{
-					write(1, *&arg, 1);
+					write(1, arg, 1);
 					arg++;
 				}
-
-				break;
 			}
-			case '%':
+			else if (*format == '%')
 			{
 				char percent = '%';
 
 				write(1, &percent, 1);
-				break;
 			}
-			case 'd':
-			case 'i':
+			else
 			{
-				int arg;
-
-				arg = va_arg(args, int);
-
-				write(1, &arg, 1);
-
-				break;
-			}
-			default:
-				write(1, format, 1);
+				write(1, format - 1,
+				      1); /* Print the '%' character */
+				write(1, format,
+				      1); /* Print the unrecognized character */
 			}
 		}
 		else
 		{
 			write(1, format, 1);
 		}
+
 		format++;
 	}
+
 	va_end(args);
-	return (0);
+	return 0;
 }
-
-
