6d53037fd2c992e43148425b79bca85490482f87
BnKeTia <bernardnketia51@gmail.com>
2023-08-19 23:23:25 +0000

    null edits

--- _printf.c
+++ _printf.c
@@ -1,63 +1,56 @@
-#include "main.h"
-#include <stdarg.h>
 #include <stdio.h>
+#include <stdarg.h>
+#include "main.h"
 #include <unistd.h>
+
 /**
- * _printf - Custom printf function
- * @format: Format string
- * @...: Variable arguments
- * Return: Number of characters printed (excluding null byte)
+ * _printf - A function that prints to the standard output.
+ * @format: format string
+ * @...: variable arguments
+ * Return: number of characters printed
  */
 int _printf(const char *format, ...)
 {
-	va_list args;
+	va_list bdl;
+	int ch = 0;
 
-	va_start(args, format);
-
+	va_start(bdl, format);
 	while (*format)
 	{
+
 		if (*format == '%')
 		{
 			format++;
-
 			if (*format == 'c')
 			{
-				int arg = va_arg(args, int);
-
+				int arg = va_arg(bdl, int);
 				write(1, &arg, 1);
+				ch++;
 			}
 			else if (*format == 's')
 			{
-				char *arg = va_arg(args, char*);
-
+				char *arg = va_arg(bdl, char*);
 				while (*arg)
 				{
 					write(1, arg, 1);
 					arg++;
+					ch++;
 				}
 			}
 			else if (*format == '%')
 			{
 				char percent = '%';
-
 				write(1, &percent, 1);
-			}
-			else
-			{
-				write(1, format - 1,
-				      1); /* Print the '%' character */
-				write(1, format,
-				      1); /* Print the unrecognized character */
+				ch++;
 			}
 		}
 		else
 		{
 			write(1, format, 1);
+			ch++;
 		}
-
-		format++;
 	}
-
-	va_end(args);
-	return 0;
+		format++;
+		va_end(bdl);
+		return (ch++);
 }
