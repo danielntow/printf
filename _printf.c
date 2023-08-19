9c9061cf779d674366f1ec61f9d6933aeadb353b
BnKeTia <bernardnketia51@gmail.com>
2023-08-19 20:06:51 +0000

    source file

---
+++ _printf.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdarg.h>
+
+
+/**
+ * _printf - A function that prints to the standard output.
+ * @format: pointer to a constant char
+ * @str: pointer to a string
+ * Return: 0 (complete)
+ */
+
+int _printf(const char *format, ...)
