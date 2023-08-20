b625da8d8aa4378e008badacd4a916c8ed26ea1c
danielntow <danielntow@gmail.com>
2023-08-20 22:38:18 +0000

    handling edge cases

--- check_edge_cases.c
+++ check_edge_cases.c
@@ -1,11 +1,71 @@
 #include "main.h"
 #include <stdarg.h>
 #include <stdio.h>
+#include "main.h"
+#include <stdarg.h>
+
+#define UNUSED(x) (void)(x)
+
+/**
+ * handle_c_edge_case - Handle edge cases for 'c' specifier.
+ * @format: Pointer to the format specifier in the format string.
+ * @args: The variable arguments list.
+ * Return: (0).
+ */
+int handle_c_edge_case(const char **format, va_list args)
+{
+	UNUSED(format);
+	va_arg(args, int); /* Consume argument for 'c' specifier */
+	return (0);
+}
+
+/**
+ * handle_s_edge_case - Handle edge cases for 's' specifier.
+ * @format: Pointer to the format specifier in the format string.
+ * @args: The variable arguments list.
+ * Return: (0).
+ */
+int handle_s_edge_case(const char **format, va_list args)
+{
+	UNUSED(format);
+	char *str;
+
+	str = va_arg(args, char*);
+	if (str == NULL)
+	{
+		_printf("(null)");
+	}
+	return (0);
+}
+
+/**
+ * handle_percent_edge_case - Handle edge cases for '%' specifier.
+ * @format: Pointer to the format specifier in the format string.
+ * @args: The variable arguments list.
+ * Return: (0).
+ */
+int handle_percent_edge_case(const char **format, va_list args)
+{
+	UNUSED(format);
+	va_arg(args, int); /* Consume argument for '%' specifier */
+
+	if (*(*format + 1) == '%')
+	{
+		(*format)++; // Move past the second '%' character
+	}
+	else if (*(*format + 1) == 'n')
+	{
+		va_arg(args, int*); // Consume argument for '%n' specifier
+	}
+
+	return (0);
+}
+
 /**
  * check_edge_cases - Check for edge cases in the format string.
  * @format: Pointer to the format specifier in the format string.
  * @args: The variable arguments list.
- * Return: -1 if an edge case is encountered, 0 otherwise.
+ * Return: (-1) if an edge case is encountered, (0) otherwise.
  */
 int check_edge_cases(const char **format, va_list args)
 {
@@ -21,20 +81,15 @@ int check_edge_cases(const char **format, va_list args)
 	}
 	else if (**format == 'c')
 	{
-		va_arg(args, int); /* Consume argument for 'c' specifier */
+		return (handle_c_edge_case(format, args));
 	}
 	else if (**format == 's')
 	{
-		char *str = va_arg(args, char *);
-
-		if (str == NULL)
-		{
-			_printf("(null)");
-		}
+		return (handle_s_edge_case(format, args));
 	}
 	else if (**format == '%')
 	{
-		va_arg(args, int); /* Consume argument for '%' specifier */
+		return (handle_percent_edge_case(format, args));
 	}
 	else
 	{
@@ -44,5 +99,6 @@ int check_edge_cases(const char **format, va_list args)
 
 	return (0);
 }
+
 
 
