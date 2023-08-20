d9ad6edb003f86fbae3763280d26eeb95b91dfa6
danielntow <danielntow@gmail.com>
2023-08-20 22:41:11 +0000

    handling edge cases

--- check_edge_cases.c
+++ check_edge_cases.c
@@ -1,104 +1,46 @@
 #include "main.h"
 #include <stdarg.h>
 #include <stdio.h>
-#include "main.h"
-#include <stdarg.h>
-
-#define UNUSED(x) (void)(x)
-
 /**
- * handle_c_edge_case - Handle edge cases for 'c' specifier.
- * @format: Pointer to the format specifier in the format string.
- * @args: The variable arguments list.
- * Return: (0).
- */
-int handle_c_edge_case(const char **format, va_list args)
-{
-	UNUSED(format);
-	va_arg(args, int); /* Consume argument for 'c' specifier */
-	return (0);
-}
-
-/**
- * handle_s_edge_case - Handle edge cases for 's' specifier.
- * @format: Pointer to the format specifier in the format string.
- * @args: The variable arguments list.
- * Return: (0).
- */
-int handle_s_edge_case(const char **format, va_list args)
-{
-	UNUSED(format);
-	char *str;
-
-	str = va_arg(args, char*);
-	if (str == NULL)
-	{
-		_printf("(null)");
-	}
-	return (0);
-}
-
-/**
- * handle_percent_edge_case - Handle edge cases for '%' specifier.
- * @format: Pointer to the format specifier in the format string.
- * @args: The variable arguments list.
- * Return: (0).
- */
-int handle_percent_edge_case(const char **format, va_list args)
-{
-	UNUSED(format);
-	va_arg(args, int); /* Consume argument for '%' specifier */
-
-	if (*(*format + 1) == '%')
-	{
-		(*format)++; // Move past the second '%' character
-	}
-	else if (*(*format + 1) == 'n')
-	{
-		va_arg(args, int*); // Consume argument for '%n' specifier
-	}
-
-	return (0);
-}
-
-/**
- * check_edge_cases - Check for edge cases in the format string.
- * @format: Pointer to the format specifier in the format string.
- * @args: The variable arguments list.
- * Return: (-1) if an edge case is encountered, (0) otherwise.
- */
+* check_edge_cases - Check for edge cases in the format string.
+* @format: Pointer to the format specifier in the format string.
+* @args: The variable arguments list.
+* Return: -1 if an edge case is encountered, 0 otherwise.
+*/
 int check_edge_cases(const char **format, va_list args)
 {
-	if (**format == ' ')
-	{
+if (**format == ' ')
+{
 		(*format)++;
 		return (-1);
-	}
-	else if (**format == '\0')
-	{
+}
+else if (**format == '\0')
+{
 		(*format)++;
 		return (-1);
-	}
-	else if (**format == 'c')
-	{
-		return (handle_c_edge_case(format, args));
-	}
-	else if (**format == 's')
-	{
-		return (handle_s_edge_case(format, args));
-	}
-	else if (**format == '%')
-	{
-		return (handle_percent_edge_case(format, args));
-	}
-	else
-	{
+}
+else if (**format == 'c')
+{
+		va_arg(args, int); /* Consume argument for 'c' specifier */
+}
+else if (**format == 's')
+{
+		char *str = va_arg(args, char *);
+
+		if (str == NULL)
+		{
+				_printf("(null)");
+		}
+}
+else if (**format == '%')
+{
+		va_arg(args, int); /* Consume argument for '%' specifier */
+}
+else
+{
 		/* Invalid format specifier, print a warning message */
 		_printf("Warning: Invalid format specifier '%c'\n", **format);
-	}
-
-	return (0);
 }
 
-
-
+return (0);
+}
--- main.h
+++ main.h
@@ -2,17 +2,33 @@
 #define MAIN_H
 
 #include <stdarg.h>
+#include <stdio.h>
 #include <unistd.h>
 
-/* Function prototypes */
-int _printf(const char *format, ...);
-void process_format(const char **format, va_list args);
-int print_string(const char *str);
-void print_char(char c);
-void print_percent(void);
+/* _putchar.c */
 int _putchar(char c);
+
+/* print_string.c */
+int print_string(const char *str);
+
+/* print_char.c */
+void print_char(char c);
+
+/* print_percent.c */
+void print_percent(void);
+
+/* process_format.c */
+void process_format(const char **format, va_list args);
+
+/* check_edge_cases.c */
 int check_edge_cases(const char **format, va_list args);
-/* Restore the args parameter */
+int handle_c_edge_case(const char **format, va_list args);
+int handle_s_edge_case(const char **format, va_list args);
+int handle_percent_edge_case(const char **format, va_list args);
+
+/* _printf.c */
+int _printf(const char *format, ...);
 
 #endif /* MAIN_H */
+
 
