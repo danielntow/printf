6909a98040db386b13f331838c1a890fbbc4bb0c
danielntow <danielntow@gmail.com>
2023-08-21 12:15:27 +0000

    handling edge case2

--- process_format.c
+++ process_format.c
@@ -9,33 +9,47 @@
  * @format: Pointer to the format specifier in the format string.
  * @args: The variable arguments list.
  */
-
 void process_format(const char **format, va_list args)
 {
-	int width, precision; /* Width and precision for 's' specifier */
-
+	int width, precision;
 	(*format)++;
 	if (**format == '%')
-		*(*format + 1) == '%' ? ((*format)++, print_char('%'))
-				      : print_percent();
+	{
+		if (*(*format + 1) == '%')
+		{
+			(*format)++;
+			print_char('%');
+		}
+		else
+			print_percent();
+	}
 	else if (**format == 'c')
+	{
 		handle_c_edge_case(format, args);
+	}
 	else if (**format == 's')
 	{
 		(*format)++;
-		width = 0, precision = -1;
+		width = 0;
+		precision = -1;
 		while (isdigit(**format))
-			width = width * 10 + (**format - '0'), (*format)++;
+		{
+			width = width * 10 + (**format - '0');
+			(*format)++;
+		}
 		if (**format == '.')
 		{
 			(*format)++;
+			precision = 0;
 			while (isdigit(**format))
-				precision = precision * 10 + (**format - '0'),
+			{
+				precision = precision * 10 + (**format - '0');
 				(*format)++;
+			}
 		}
 		handle_s_edge_case(format, args, width, precision);
 	}
 	else
-		print_char('%'), print_char(**format);
+		print_char(**format);
 }
 
