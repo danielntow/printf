abebdebf6c515f8fe20c5ce6c91392c65561d7fa
BnKeTia <bernardnketia51@gmail.com>
2023-08-21 00:26:27 +0000

    creating a test file for all edge cases

--- test_files/main.c
+++ test_files/main.c
@@ -2,8 +2,21 @@
 
 int main(void)
 {
-    // Call your _printf function here or perform other operations
+	char *name = "bnketia";
+	char ch = 'a';
+	char *name_partner = "bnketia/dasare";
+	int num = 12;
+
     _printf("Hello, %s!\n", "world");
+    _printf("testprint\n");
+    _printf("hello, %s\n", name);
+    _printf("one %%");
+    _printf("first letter is: %c\n", ch);
+    _printf("two %%%c\n", ch);
+    _printf("hello %shi\n", name);
+    _printf("hola %s\t", name);
+    _printf("my age is: %d\n", num);
+    _printf("which of the devs do u need: %s\n", name_partner);
 
     return 0;
 }
